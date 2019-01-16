223f9a571156400926d607a71ef4546d219c80c0
Ashok Reddy <ashokreddy@ls-phy-lap-0113.local>
2019-01-16 12:01:24 +0530

    This is repositoryDemo is the first project

---
+++ RepositoryDemo/RepositoryDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RepositoryDemo
+//
+//  Created by Ashok Reddy on 16/01/19.
+//  Copyright © 2019 Ashok Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
